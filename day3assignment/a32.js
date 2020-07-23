15a7381427e7804179f1711877ed94a70f34493a
Aaryan199 <68271227+Aaryan199@users.noreply.github.com>
2020-07-23 20:28:38 +0530

    Add files via upload

---
+++ day3assignment/a32.js
@@ -0,0 +1,8 @@
+var y=prompt("What is the OS you use?")
+z= y.split(" ")
+//console.log("The OS is ${z}","the version is ${z}")
+//z=y.slice("0,6")
+ console.log(z[0],z[1])
+// p=z[0]
+// console.log(p)
+ console.log("The OS name is z[0], and version is z[1]");
---
+++ day3assignment/grade.js
@@ -0,0 +1,26 @@
+let val = prompt("What is your score?:");
+console.log(val)
+if (val<=10){
+  console.log("Grade is f")
+}
+else if (val<=2) {
+  console.log("Grade is f")
+}
+else if (val<=30) {
+  console.log("Grade is f")
+}
+else if (val<=40) {
+  console.log("Grade is f")
+}
+else if (val<=50) {
+  console.log("Grade is D")
+}
+else if (val<=60) {
+  console.log("Grade is c")
+}
+else if (val<=80) {
+  console.log("Grade is B")
+}
+else if (val<=100) {
+  console.log("Grade is A")
+}
